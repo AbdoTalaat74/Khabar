8f6207f358daad87f2dfa82bd20b9f3409dd86fa
ABDOO <Abdo.talaat740@gmail.com>
2024-11-17 19:50:28 +0200

    OnBoardingScreen Finished

--- app/src/main/java/com/loc/newsapp/MainActivity.kt
+++ app/src/main/java/com/loc/newsapp/MainActivity.kt
@@ -3,7 +3,10 @@ package com.loc.newsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -11,6 +14,11 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
+import com.loc.newsapp.presentation.onboarding.OnBoardingScreen
+import com.loc.newsapp.ui.theme.LightBlack
 import com.loc.newsapp.ui.theme.NewsAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,24 +28,18 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
+        WindowCompat.setDecorFitsSystemWindows(window, true)
+        installSplashScreen()
         setContent {
-            Surface(
-                modifier = Modifier.fillMaxSize(),
-                color = MaterialTheme.colorScheme.background
-            ) {
-                NewsAppTheme {
-                    Column(
-                        modifier = Modifier.fillMaxSize(),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Text(text = "Hello Talaat")
-                    }
+            NewsAppTheme {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(if (isSystemInDarkTheme()) LightBlack else Color.White),
+                ) {
+                    OnBoardingScreen()
                 }
             }
-
         }
     }
 }
